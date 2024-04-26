991e7d7b590728b716108493660e7de12e790144
Serena Nguyen <nguyenserenaa@gmail.com>
2024-04-26 09:48:41 -0700

    test commit 1

--- src/MovieListServlet.java
+++ src/MovieListServlet.java
@@ -1,2 +1,137 @@
-package PACKAGE_NAME;public class MovieListServlet {
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+// Declaring a WebServlet called SingleStarServlet, which maps to url "/api/single-star"
+@WebServlet(name = "MovieListServlet", urlPatterns = "/api/movie-list")
+public class MovieListServlet extends HttpServlet {
+    private static final long serialVersionUID = 2L;
+
+    // Create a dataSource which registered in web.xml
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/json"); // Response mime type
+
+        // Retrieve parameter id from url request.
+        String id = request.getParameter("id");
+
+        // The log message can be found in localhost log
+        request.getServletContext().log("getting id: " + id);
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        // Get a connection from dataSource and let resource manager close the connection after usage.
+        try (Connection conn = dataSource.getConnection()) {
+            // Get a connection from dataSource
+
+            // Construct a query with parameter represented by "?"
+            String query = "SELECT \n" +
+                    "m.title,\n" +
+                    "m.year,\n" +
+                    "m.director,\n" +
+                    "GROUP_CONCAT(DISTINCT g.name ORDER BY g.genreId) AS genres,\n" +
+                    "GROUP_CONCAT(DISTINCT s.name ORDER BY s.starId) AS stars,\n" +
+                    "r.rating\n" +
+                    "FROM \n" +
+                    "movies m\n" +
+                    "LEFT JOIN \n" +
+                    "genres_gitin_movies gim ON m.id = gim.movieId\n" +
+                    "LEFT JOIN \n" +
+                    "genres g ON gim.genreId = g.id\n" +
+                    "LEFT JOIN \n" +
+                    "stars_in_movies sim ON m.id = sim.movieId\n" +
+                    "LEFT JOIN \n" +
+                    "stars s ON sim.starId = s.id\n" +
+                    "LEFT JOIN \n" +
+                    "ratings r ON m.id = r.movieId\n" +
+                    "GROUP BY \n" +
+                    "m.title, m.year, m.director, r.rating;";
+
+            // Declare our statement
+            PreparedStatement statement = conn.prepareStatement(query);
+
+            // Set the parameter represented by "?" in the query to the id we get from url,
+            // num 1 indicates the first "?" in the query
+            statement.setString(1, id);
+
+            // Perform the query
+            ResultSet rs = statement.executeQuery();
+
+            JsonArray jsonArray = new JsonArray();
+
+            // Iterate through each row of rs
+            while (rs.next()) {
+                String movieId = rs.getString("id"); // Corresponds to m.id in the SQL query
+                String movieTitle = rs.getString("title"); // Corresponds to m.title in the SQL query
+                String movieYear = rs.getString("year"); // Corresponds to m.year in the SQL query
+                String movieDirector = rs.getString("director"); // Corresponds to m.director in the SQL query
+                String genres = rs.getString("genres"); // Corresponds to the result of GROUP_CONCAT(DISTINCT g.name ORDER BY g.genreId) AS genres in the SQL query
+                String stars = rs.getString("stars"); // Corresponds to the result of GROUP_CONCAT(DISTINCT s.name ORDER BY s.starId) AS stars in the SQL query
+                double rating = rs.getDouble("rating"); // Corresponds to r.rating in the SQL query
+
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("movie_id", movieId);
+                jsonObject.addProperty("movie_title", movieTitle);
+                jsonObject.addProperty("movie_year", movieYear);
+                jsonObject.addProperty("movie_director", movieDirector);
+                jsonObject.addProperty("genres", genres);
+                jsonObject.addProperty("stars", stars);
+                jsonObject.addProperty("rating", rating);
+
+                jsonArray.add(jsonObject);
+            }
+            rs.close();
+            statement.close();
+
+            // Write JSON string to output
+            out.write(jsonArray.toString());
+            // Set response status to 200 (OK)
+            response.setStatus(200);
+
+        } catch (Exception e) {
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Log error to localhost log
+            request.getServletContext().log("Error:", e);
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+
+        // Always remember to close db connection after usage. Here it's done by try-with-resources
+
+    }
+
 }
